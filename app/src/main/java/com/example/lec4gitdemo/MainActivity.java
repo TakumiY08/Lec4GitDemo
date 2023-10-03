4e7cb93dfa2dcc9cd7419dfa273e1d9d49f25a5e
TakumiY08 <134025677+TakumiY08@users.noreply.github.com>
2023-10-03 10:03:16 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/lec4gitdemo/MainActivity.java
+++ app/src/main/java/com/example/lec4gitdemo/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Made some changes locally the following week
+    //Another changes for the test
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
